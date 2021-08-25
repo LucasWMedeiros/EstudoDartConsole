ebb9daf5152e9e2d7e2027c03499dc0a23e90f57
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2021-08-25 19:49:07 -0300

    Update aviao.dart

--- lib/aviao.dart
+++ lib/aviao.dart
@@ -2,21 +2,6 @@ import 'package:atividades_console/assentos.dart';
 
 class Aviao {
   List<Assentos> assentoTotal = [];
-/*
-  Aviao() {
-    inicilizarLista();
-  }
-  Assentos? assento;
-
- 
-
-void inicilizarLista() {
-  for (int i = 1; i > 25; i++) {
-    assentoTotal?[i].setId(i);
-    assentoTotal?[i].setVago(false);
-  }
-}
-*/
 
 void setList(List<Assentos> lista){
   assentoTotal = lista;
